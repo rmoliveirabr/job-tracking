145dda74abeb2d571a9cf03472fbd73e3aa86b52
rmoliveirabr <rmoliveirabr@gmail.com>
2023-08-29 19:34:46 +0200

    Removing Resume code

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { AppComponent } from './app.component';
 import { ProfileFormComponent } from './components/profile-form/profile-form.component';
 import { ProfileListComponent } from './components/profile-list/profile-list.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ResumeListComponent } from './components/resume-list/resume-list.component';
-import { ResumeFormComponent } from './components/resume-form/resume-form.component';
 import { ProfileViewComponent } from './components/profile-view/profile-view.component';
 
 @NgModule({
@@ -17,8 +15,6 @@ import { ProfileViewComponent } from './components/profile-view/profile-view.com
     ProfileFormComponent,
     ProfileListComponent,
     NavbarComponent,
-    ResumeListComponent,
-    ResumeFormComponent,
     ProfileViewComponent
   ],
   imports: [
